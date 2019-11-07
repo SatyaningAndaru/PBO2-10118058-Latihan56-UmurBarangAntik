8d9ab3fb4c4460ab8a0e5553c4c4ddaeef5c2511
SatyaningAndaru <55575075+SatyaningAndaru@users.noreply.github.com>
2019-11-07 21:29:57 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118058/latihan56/umurbarangantik/PBO210118058Latihan56UmurBarangAntik.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118058.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author user
+ *  Nama:Satyaning Andaru bawalaksana
+     * Nim: 10118058
+     * kelas : If-2
+ */
+public class PBO210118058Latihan56UmurBarangAntik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Radio r = new Radio(234);
+        r.setName("Radio AM");
+        System.out.println("Nama barang Antik : "+r.getName());
+        r.tampilUmur();
+    }
+    
+}
---
+++ src/pbo2/pkg10118058/latihan56/umurbarangantik/Radio.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118058.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author user
+ */
+public class Radio extends BarangAntik {
+    private String name;
+
+    public Radio(int umur) {
+        super(umur);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void tampilUmur() {
+        super.tampilUmur(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
